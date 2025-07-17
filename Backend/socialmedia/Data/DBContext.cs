a208a2adf341a1789770f4bafa77f0057a3a237f
CTRL02 <philo.matta@gmail.com>
2025-07-17 20:44:24 +0300

    monorepo structure

---
+++ Backend/socialmedia/Data/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace socialmedia.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<Entities.AppUser> Users { get; set; }
+ 
+    }
+}
